2d21bbcf4e172534fddfb82855d1d7e67036bfb0
Principe <36563911+BiaPri@users.noreply.github.com>
2022-02-14 17:36:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-import os
-from setuptools import setup, find_packages, Command
-
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system('del ./*.egg-info')
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     content = f.readlines()
@@ -21,5 +7,4 @@ requirements = [x.strip() for x in content]
 setup(name='ask_nature_scraper',
       description="package needed to scrape",
       package=find_packages(),
-      install_requires=requirements,
-      cmdclass={'clean': CleanCommand})
+      install_requires=requirements)
